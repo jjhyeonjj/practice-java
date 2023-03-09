8bcc291407a63b29140e9f4ccd81c53fa2c5f652
jjhyeonjj <jjh4523a@gmail.com>
2023-03-09 23:51:13 +0900

    명품 자바 프로그래밍 0202 예제

---
+++ practice-java/src/kr/co/ezenac/mjp/mjp02/h/H0201.java
@@ -0,0 +1,17 @@
+package kr.co.ezenac.mjp.mjp02.h;
+
+import java.util.Scanner;
+
+public class H0201 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("원화를 입력하세요(단위 원) >> ");
+		int won = scan.nextInt();
+		
+		double dollar = won / 1100;
+		System.out.println(won+"원은 "+"$"+dollar+"입니다.");
+		
+		scan.close();
+	}
+}
